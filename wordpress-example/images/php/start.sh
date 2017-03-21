26e22f22c148e1a76a6ebd926a705ddf0004759d
server4001 <server4001@gmail.com>
2017-03-20 21:08:47 -0700

    Changes wordpress example so that it uses PHP7

--- wordpress-example/images/php/start.sh
+++ wordpress-example/images/php/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 usermod -u 1000 www-data
-php5-fpm -c /etc/php5/fpm/php.ini --fpm-config /etc/php5/fpm/php-fpm.conf
+php-fpm7.0 -c /etc/php/7.0/fpm/php.ini --fpm-config /etc/php/7.0/fpm/php-fpm.conf

e7ec35f84a70fa87e920a852b2e2a1519dc27ee9
Brice Bentler <server4001@gmail.com>
2017-07-09 21:51:25 -0700

    Adds health check to silex-alpine example

---
+++ silex-alpine-example/public/health.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+header('Content-Type: application/json');
+http_response_code(200);
+
+echo json_encode([
+    'status' => 'success',
+    'data' => [
+        'php_version' => PHP_VERSION,
+        'hostname' => gethostname(),
+    ],
+]);
